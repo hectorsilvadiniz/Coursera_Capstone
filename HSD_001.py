9d55dafef6482288587406e685dcdb14c7a61f2f
hectorsilvadiniz <49822586+hectorsilvadiniz@users.noreply.github.com>
2019-06-13 21:18:23 -0300

    Add files via upload

---
+++ HSD_001.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## This notebook will be used for Capstone Project
+
+# In[1]:
+
+
+
+import pandas as pd
+import numpy as np
+
+print("Hello Capstone Project Course!")
+
